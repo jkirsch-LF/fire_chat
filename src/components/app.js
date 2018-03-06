e1341c1f3e25d7ed0d3fa02fab32af17000cf772
James Kirsch <jameskirschaf@gmail.com>
2018-03-05 17:40:41 -0800

    Finished

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,20 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import Chat from './chat';
 import Lobby from './lobby';
+import ChatRoom from './chat_room';
+
+import { Route } from 'react-router-dom';
+
+
 
 const App = () => (
     <div>
         <div className="container">
             <h1 className="center-align">Fire 🔥 Chat</h1>
-            <Lobby/>
+
+            <Route exact path="/" component={Lobby} />
+            <Route path="/room/:roomId/log/:logId" component={ChatRoom} />
+
         </div>
     </div>
 );
--- src/components/lobby.js
+++ src/components/lobby.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { db } from '../firebase';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getRoomList, createRoom } from '../actions';
 
 class Lobby extends React.Component {
 
@@ -8,42 +11,65 @@ class Lobby extends React.Component {
 
         this.state = {
             roomName: ''
-        }
+        };
+
+        this.dbChatRef = db.ref('/chat-rooms');
     }
 
+    componentDidMount() {
+        this.dbChatRef.on('value', snapshot => {
+            this.props.getRoomList(snapshot.val());
+        });
+    }
+
+    componentWillUnmount() {
+        this.dbChatRef.off();
+    }
 
     handleCreateRoom(e) {
-        const { roomName } = this.state;
-
         e.preventDefault();
-
-        console.log('Room Name: ', roomName);
-
-        const newRoom = {
-            name: roomName,
-            chatLog: [`Room: ${roomName}`]
-        }
-
-        db.ref('/chat-rooms')
-            .push(newRoom)
-            .then(resp =>{
-                console.log('Add Room Response: ', resp);
-            });
+        this.props.createRoom(this.state.roomName);
+        this.setState({ roomName: '' });
     }
 
     render() {
         const { roomName } = this.state;
+        const { roomList } = this.props;
+
+        let rooms = [];
+
+        if (roomList) {
+            rooms = Object.keys(roomList).map((key, index) => {
+                return (
+                    <li key={index} className="collection-item">
+                        <Link to={`/room/${key}/log/${roomList[key].chatLogId}`}>{roomList[key].name}</Link>
+                    </li>
+                )
+            });
+        } else {
+            rooms.push(<li key="0" className="collection-item">No rooms available.</li>)
+        }
+
         return (
             <div>
                 <h3>Chat Lobby</h3>
                 <form onSubmit={this.handleCreateRoom.bind(this)}>
                     <label>Chat Room Name</label>
                     <input type="text" onChange={e => this.setState({ roomName: e.target.value })} value={roomName} />
-                    <button>Create Room</button>
+                    <button className="btn red">Create Room</button>
                 </form>
+                <ul className="collection">
+                    {rooms}
+                </ul>
             </div>
         )
     }
 }
 
-export default Lobby;
+function mapStateToProps(state) {
+    return {
+        roomList: state.chat.roomList
+    }
+}
+
+export default connect(mapStateToProps, { getRoomList, createRoom })(Lobby);
